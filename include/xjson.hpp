3c3a62784124ed2e8b9e18f5ad76c3c7f93ea284
fuwangqin <382018309@qq.com>
2016-11-15 14:23:18 +0800

    add codes

--- include/xjson.hpp
+++ include/xjson.hpp
@@ -9,9 +9,8 @@
 
 namespace xjson
 {
-
 	#define HAS_MEMBER(member)\
-	template<typename T, typename... Args>struct has_member_##member\
+	template<typename T, typename... Args> struct has_member_##member\
 	{\
 	private:\
 			template<typename U> static auto Check(int) ->\
@@ -23,6 +22,102 @@ namespace xjson
 
 	HAS_MEMBER(xpack)
 	HAS_MEMBER(xunpack)
+
+
+	#define MARCO_EXPAND(...)                 __VA_ARGS__
+	#define MAKE_ARG_LIST_1(op, arg, ...)   op(arg)
+	#define MAKE_ARG_LIST_2(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_1(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_3(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_2(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_4(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_3(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_5(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_4(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_6(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_5(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_7(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_6(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_8(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_7(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_9(op, arg, ...)   op(arg) MARCO_EXPAND(MAKE_ARG_LIST_8(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_10(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_9(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_11(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_10(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_12(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_11(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_13(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_12(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_14(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_13(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_15(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_14(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_16(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_15(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_17(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_16(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_18(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_17(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_19(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_18(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_20(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_19(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_21(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_20(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_22(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_21(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_23(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_22(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_24(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_23(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_25(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_24(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_26(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_25(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_27(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_26(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_28(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_27(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_29(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_28(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_30(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_29(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_31(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_30(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_32(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_31(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_33(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_32(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_34(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_33(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_35(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_34(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_36(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_35(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_37(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_36(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_38(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_37(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_39(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_38(op, __VA_ARGS__))
+	#define MAKE_ARG_LIST_40(op, arg, ...)  op(arg) MARCO_EXPAND(MAKE_ARG_LIST_39(op, __VA_ARGS__))
+
+	#define XPACK_OBJECT(Obj) o[#Obj]=Obj;
+	#define XUNPACK_OBJECT(Obj) Obj = o[#Obj].get<decltype(Obj)>();
+
+	#define GEN_XPACK_OBJECT(...) void xpack (xjson::obj_t &o) const { __VA_ARGS__; }
+	#define GEN_XUNPACK_OBJECT(...) void xunpack(obj_t &o) { __VA_ARGS__; }
+
+	#define MACRO_CONCAT(A, B)  A##_##B
+
+	#define MAKE_ARG_LIST(N, op, arg, ...) \
+			MACRO_CONCAT(MAKE_ARG_LIST, N)(op, arg, __VA_ARGS__)
+
+	#define GEN_XPACK_FUNC(N, ...) \
+	GEN_XPACK_OBJECT(MAKE_ARG_LIST(N, XPACK_OBJECT, __VA_ARGS__)) 
+
+	#define GEN_XUNPACK_FUNC(N, ...) \
+	GEN_XUNPACK_OBJECT(MAKE_ARG_LIST(N, XUNPACK_OBJECT, __VA_ARGS__)) 
+
+	#define RSEQ_N() \
+			 119,118,117,116,115,114,113,112,111,110,\
+			 109,108,107,106,105,104,103,102,101,100,\
+			 99,98,97,96,95,94,93,92,91,90, \
+			 89,88,87,86,85,84,83,82,81,80, \
+			 79,78,77,76,75,74,73,72,71,70, \
+			 69,68,67,66,65,64,63,62,61,60, \
+			 59,58,57,56,55,54,53,52,51,50, \
+			 49,48,47,46,45,44,43,42,41,40, \
+			 39,38,37,36,35,34,33,32,31,30, \
+			 29,28,27,26,25,24,23,22,21,20, \
+			 19,18,17,16,15,14,13,12,11,10, \
+			 9,8,7,6,5,4,3,2,1,0
+
+	#define ARG_N( \
+			 _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+			 _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+			 _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+			 _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+			 _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+			 _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+			 _61,_62,_63,_64,_65,_66,_67,_68,_69,_70, \
+			 _71,_72,_73,_74,_75,_76,_77,_78,_79,_80, \
+			 _81,_82,_83,_84,_85,_86,_87,_88,_89,_90, \
+			 _91,_92,_93,_94,_95,_96,_97,_98,_99,_100, \
+			 _101,_102,_103,_104,_105,_106,_107,_108,_109,_110, \
+			 _111,_112,_113,_114,_115,_116,_117,_118,_119,N, ...) N
+
+	#define GET_ARG_COUNT_INNER(...)    MARCO_EXPAND(ARG_N(__VA_ARGS__))
+	#define GET_ARG_COUNT(...)          GET_ARG_COUNT_INNER(__VA_ARGS__, RSEQ_N())
+
+	#define XPACK(...) GEN_XPACK_FUNC(GET_ARG_COUNT(__VA_ARGS__), __VA_ARGS__)
+	#define XUNPACK(...) GEN_XUNPACK_FUNC(GET_ARG_COUNT(__VA_ARGS__), __VA_ARGS__)
+
+	#define  XGSON(...) XPACK(__VA_ARGS__) XUNPACK(__VA_ARGS__)
 
 	class type_error: public std::exception
 	{
@@ -46,6 +141,74 @@ namespace xjson
 	};
 
 	#define xjson_assert(x) if(!(x)) throw type_error(__FILE__, __LINE__, #x);
+	
+	struct null
+	{
+	};
+
+	template<typename T>
+	struct optional
+	{
+		using type = T;
+
+		optional() = default;
+		optional(const T &t)
+		{
+			val_ = t;
+		}
+		operator bool()
+		{
+			return valid_;
+		}
+		bool operator !()
+		{
+			return !valid_;
+		}
+
+		const T &get() const
+		{
+			return val_;
+		}
+		void operator=(const T &val)
+		{
+			valid_ = true;
+			val_ = val;
+		}
+		optional &operator = (optional <T> &&val)
+		{
+			if (val.valid_)
+			{
+				val_ = std::move(val.val_);
+				valid_ = true;
+			}
+			return  *this;
+		}
+		optional &operator = (const optional &val)
+		{
+			if (val.valid_)
+			{
+				val_ = val.val_;
+				valid_ = true;
+			}
+			return *this;
+		}
+		optional(const optional & val)
+		{
+			if (val.valid_)
+			{
+				val_ = val.val_;
+				valid_ = true;
+			}
+		}
+	private:
+		bool valid_ = false;
+		T val_;
+	};
+	template<typename T>
+	struct is_optional :std::false_type {};
+
+	template<typename T>
+	struct is_optional<optional<T>> :std::true_type {};
 
 	struct obj_t
 	{
@@ -71,9 +234,8 @@ namespace xjson
 			std::vector<obj_t*> *vec_;
 		} val_;
 
-		struct null 
-		{
-		};
+		
+
 		obj_t()
 			:type_(e_null)
 		{
@@ -108,6 +270,45 @@ namespace xjson
 			return *this;
 		}
 
+		template<typename T>
+		obj_t & operator = (const std::vector<T> &o)
+		{
+			for (auto &itr: o)
+			{
+				add(itr);
+			}
+			return *this;
+		}
+		template<typename T>
+		obj_t & operator = (const std::list<T> &o)
+		{
+			for (auto &itr : o)
+			{
+				add(itr);
+			}
+			return *this;
+		}
+		template<typename T>
+		obj_t &operator=(std::map<std::string, T> && vals)
+		{
+			for (auto &itr: vals)
+			{
+				operator[](itr.first) = itr.second;
+			}
+			return *this;
+		}
+		template<typename T>
+		obj_t &operator=(optional<T> && val)
+		{
+			*this = std::move(val.get());
+			return *this;
+		}
+		template<typename T>
+		obj_t &operator=(const optional<T> & val)
+		{
+			*this = val.get();
+			return *this;
+		}
 		virtual ~obj_t()
 		{
 			reset();
@@ -290,6 +491,21 @@ namespace xjson
 			T t;
 			t.xunpack(*this);
 			return std::move(t);
+		}
+		template<class T>
+		typename std::enable_if<is_optional<T>::value, T>::type
+			get()
+		{
+			T::type val;
+			try
+			{
+				val = get<T::type>();
+			}
+			catch (type_error)
+			{
+				return T();
+			}
+			return T(val);
 		}
 
 		template<typename T>
--- unit_test/test.cpp
+++ unit_test/test.cpp
@@ -10,30 +10,22 @@ XTEST_SUITE(xjson)
 		obj_t o(1);
 		xassert(o.get<int>() == 1);
 	}
-
 	struct user
 	{
 		int id;
 		std::string name;
-		void xpack(obj_t &o)const
-		{
-			o["id"] = id;
-			o["name"] = name;
-		}
-		void xunpack(obj_t &o)
-		{
-			id = o["id"].get<decltype(id)>();
-			name = o["name"].get<decltype(name)>();
-		}
+		
+		XGSON(id,name)
 	};
 
 
-	XUNIT_TEST(get_struct)
+	XUNIT_TEST(XGSON)
 	{
 		user u{ 1,"u1" };
-		
+
 		obj_t o;
 		o["user"] = u;
+		o["users"].add(u);
 
 		xassert(o["user"]["id"].get<int>() == 1);
 		xassert(o["user"]["name"].get<std::string>() == "u1");
@@ -42,5 +34,70 @@ XTEST_SUITE(xjson)
 
 		xassert(u2.id == u.id);
 		xassert(u2.name == u.name);
+
+	}
+	XUNIT_TEST(vector)
+	{
+		obj_t o;
+
+		o["vec"] = std::vector<int>{ 1,2,3,4,5 };
+
+		for (auto &ind : { 1,2,3,4,5 })
+		{
+			xassert(o["vec"].get<int>(ind - 1) == ind);
+		}
+		user u{ 1,"u1" };
+
+		o["users"].add(u);
+		xassert(o["users"].get<user>(0).id == 1 && o["users"].get<user>(0).name == "u1");
+	}
+
+	XUNIT_TEST(map)
+	{
+		obj_t o;
+		o = std::map<std::string, int>{ {"1",1},{ "2",2 } };
+
+		xassert(o["1"].get<int>() == 1);
+		xassert(o["2"].get<int>() == 2);
+
+
+		o["map"] = std::map<std::string, int>{ { "1",1 },{ "2",2 } };
+
+		xassert(o["map"]["1"].get<int>() == 1);
+		xassert(o["map"]["2"].get<int>() == 2);
+	}
+	XUNIT_TEST(list)
+	{
+
+		obj_t o;
+		o["vec"] = std::list<int>{ 1,2,3,4,5 };
+
+		for (auto &ind : { 1,2,3,4,5 })
+		{
+			xassert(o["vec"].get<int>(ind - 1) == ind);
+		}
+		user u{ 1,"u1" };
+
+		o["users"].add(u);
+		xassert(o["users"].get<user>(0).id == 1 && o["users"].get<user>(0).name == "u1");
+	}
+	XUNIT_TEST(optional)
+	{
+		optional<int> a;
+		xassert((!a));
+		a = 1;
+		xassert( (!!a) );
+		xassert(a.get() == 1);
+
+		obj_t o;
+		o["a"] = a;
+		o["b"] = optional<int>();
+
+		a = o["a"].get<decltype(a)>();
+
+		optional<int> no_exist;
+		no_exist = o["no_exist"].get<decltype(a)>();
+		xassert((!no_exist));
+
 	}
 }
